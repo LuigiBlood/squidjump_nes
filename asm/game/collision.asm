7f5596062ed0b60512cadbf1d16cb214935d959a
LuigiBlood <luigiblood@gmail.com>
2024-09-04 02:10:45 +0200

    Collision Code working with 16-bit Y Position instead

--- asm/game/collision.asm
+++ asm/game/collision.asm
@@ -23,21 +23,42 @@ _checkplatforms:
 	lda game_platform_table+3,x
 	asl; asl; asl
 	sta argument0
+	lda game_platform_table+3,x
+	lsr; lsr; lsr; lsr; lsr
+	sta argument1
+	lda game_platform_table+4,x
+	asl; asl; asl
+	ora argument1
+	sta argument1
 	//Compare Y <= Platform Y Pixel Position
 	lda squid_y_lo
 	cmp argument0
 	bcc +
 	beq +
 	jmp _skiptonextplatform
++;	lda squid_y_hi
+	cmp argument1
+	bcc +
+	beq +
+	jmp _skiptonextplatform
++;
 	//Compare Y + DY >= Platform Y Pixel Position
-+;	clc
+	lda squid_y_lo
+	clc
 	adc squid_dy_lo
 	cmp argument0
+	bcs +
+	jmp _skiptonextplatform
++;	lda squid_y_hi
+	adc #0
+	cmp argument1
 	bcs +
 	jmp _skiptonextplatform
 	//If both are true, then stop any downwards acceleration
 +;	lda argument0
 	sta squid_y_lo
+	lda argument1
+	sta squid_y_hi
 	lda #0
 	sta squid_dy_lo
 	sta squid_dy_frac
